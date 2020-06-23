751f092c33d13125ceebdbd471c49e377f4d0b07
codewolf86 <chris@codewolfconsulting.com>
2020-06-23 15:51:02 +0100

    Remove Mediatr and simplify project structure and dependencies

--- src/SFA.DAS.Tools.Support.Web/App_Start/IoC.cs
+++ src/SFA.DAS.Tools.Support.Web/App_Start/IoC.cs
@@ -1,16 +1,12 @@
-﻿using AutoMapper;
-using MediatR;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SFA.DAS.Commitments.Api.Client;
 using SFA.DAS.Commitments.Api.Client.Interfaces;
 using SFA.DAS.Http;
 using SFA.DAS.Http.TokenGenerators;
-using SFA.DAS.Tools.Support.Core.Handlers;
 using SFA.DAS.Tools.Support.Core.Models;
 using SFA.DAS.Tools.Support.Core.Services;
 using SFA.DAS.Tools.Support.Web.Configuration;
-using SFA.DAS.Tools.Support.Web.Models;
 using System.Reflection;
 
 namespace SFA.DAS.Tools.Support.Web.App_Start
@@ -19,19 +15,7 @@ namespace SFA.DAS.Tools.Support.Web.App_Start
     {
         public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration _configuration)
         {
-            services.AddScoped<IMapper>(s =>
-            {
-                var config = new MapperConfiguration(cfg =>
-                {
-                    cfg.CreateMap<StopApprenticeshipResult, StopApprenticeshipMessageResult>();
-                });
-
-                var mapper = new Mapper(config);
-                return mapper;
-            });
             services.Configure<CommitmentsApiClientConfiguration>(_configuration.GetSection("CommitmentsApiClientConfiguration"));
-            services.AddMediatR(Assembly.GetExecutingAssembly(), typeof(StopApprenticeshipMessage).Assembly);
-            services.AddTransient<IMediatorService, MediatorService>();
             services.AddTransient<IEmployerCommitmentsService, EmployerCommitmentsService>();
             services.AddScoped<IEmployerCommitmentApi, EmployerCommitmentApi>(provider =>
             {
--- src/SFA.DAS.Tools.Support.Web/Controllers/ApprovalsController.cs
+++ src/SFA.DAS.Tools.Support.Web/Controllers/ApprovalsController.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using SFA.DAS.Tools.Support.Core.Handlers;
 using SFA.DAS.Tools.Support.Core.Services;
 using SFA.DAS.Tools.Support.Web.Configuration;
 using SFA.DAS.Tools.Support.Web.Models;
-using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,13 +12,13 @@ namespace SFA.DAS.Tools.Support.Web.Controllers
     public class ApprovalsController : Controller
     {
         private readonly ILogger<ApprovalsController> _logger;
-        private readonly IMediatorService _mediator;
+        private readonly IEmployerCommitmentsService _employerCommitmentsService;
         private readonly string claimUserName = "https://tools.apprenticeships.education.gov.uk/claims/nickname";
 
-        public ApprovalsController(ILogger<ApprovalsController> logger, IMediatorService mediator)
+        public ApprovalsController(ILogger<ApprovalsController> logger, IEmployerCommitmentsService employerCommitmentsService)
         {
             _logger = logger;
-            _mediator = mediator;
+            _employerCommitmentsService = employerCommitmentsService;
         }
 
         [HttpGet("stopApprenticeship", Name = ApprovalsRouteNames.StopApprenticeship)]
@@ -46,22 +44,16 @@ namespace SFA.DAS.Tools.Support.Web.Controllers
                 return View(model);
             }
 
-            var result = await _mediator.Send<StopApprenticeshipMessage, StopApprenticeshipMessageResult>(new StopApprenticeshipMessage
-            {
-                ApprenticeshipId = model.ApprenticeshipId,
-                EmployerAccountId = model.EmployerAccountId,
-                StopDate = model.StopDate,
-                UserId = userId
-            });
+            var result = await _employerCommitmentsService.StopApprenticeship(model.EmployerAccountId, model.ApprenticeshipId, userId, model.StopDate);
 
-            if (!result.HasSuccess)
+            if (result.HasError)
             {
                 ModelState.AddModelError("", $"Call to Commitments Api Failed {result.ErrorMessage}");
                 return View(model);
             }
             else
             {
-                model.SubmittedSuccessfully = result.HasSuccess;
+                model.SubmittedSuccessfully = true;
                 return View(model);
             }
         }
