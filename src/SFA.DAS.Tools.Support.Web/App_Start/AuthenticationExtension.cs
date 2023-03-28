8200b060f08885459cccad808202450ebdc4898f
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2023-03-28 16:33:43 +0100

    Commit Details: Story: The purpose of this story is to put behind a feature toggle the DfE sign in authentication, replacing the existing Pirean, SAML based authentication. When the UseDfESignIn toggle is on -> Start now button should redirect user to DfESignIn Page
    Details
    Extended SFA.DAS.Recruit.QA Config to hold the UseDfeSignIn property. "UseDfeSignIn": true.
    Added logic/condition in AuthenticationExtension to check if the UseDfeSignIn property is set to true, so that DfESignin happens.
    Check the Authentication scheme based on UseDfeSignIn property and use the relevant sign-out Authentication Scheme.

--- src/SFA.DAS.Tools.Support.Web/App_Start/AuthenticationExtension.cs
+++ src/SFA.DAS.Tools.Support.Web/App_Start/AuthenticationExtension.cs
@@ -1,44 +1,60 @@
-﻿using Microsoft.AspNetCore.Authentication.Cookies;
+﻿using AutoMapper.Configuration;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authentication.WsFederation;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using SFA.DAS.DfESignIn.Auth.AppStart;
+using SFA.DAS.DfESignIn.Auth.Interfaces;
 using SFA.DAS.Tools.Support.Web.Configuration;
 using System;
 using System.Threading.Tasks;
+using SFA.DAS.Tools.Support.Web.Infrastructure;
+using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
 
 namespace SFA.DAS.Tools.Support.Web.App_Start
 {
     public static class AuthenticationExtension
     {
+        private const string CookieName = "SFA.DAS.ToolService.Support.Web.Auth";
         public static IServiceCollection AddAuthentication(this IServiceCollection services, IConfiguration configuration)
         {
             var authenticationConfiguration = new AuthenticationConfiguration();
             configuration.GetSection("Authentication").Bind(authenticationConfiguration);
 
-            services.AddAuthentication(options =>
+            var useDfESignIn = configuration.GetValue<bool>("UseDfESignIn");
+
+            if (useDfESignIn)
             {
-                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = WsFederationDefaults.AuthenticationScheme;
-                options.DefaultSignOutScheme = WsFederationDefaults.AuthenticationScheme;
-            })
-             .AddWsFederation(options =>
-             {
-                 options.Wtrealm = authenticationConfiguration.Wtrealm;
-                 options.MetadataAddress = authenticationConfiguration.MetadataAddress;
-                 options.UseTokenLifetime = false;
-             }).AddCookie(options =>
+                // register DfeSignIn authentication services to the AspNetCore Authentication Options.
+                services.AddAndConfigureDfESignInAuthentication(configuration, $"{CookieName}", typeof(CustomServiceRole));
+            }
+            else
             {
-                options.AccessDeniedPath = new PathString("/Error/403");
-                options.ExpireTimeSpan = TimeSpan.FromHours(1);
-                options.Cookie.Name = "SFA.DAS.ToolService.Support.Web.Auth";
-                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-                options.SlidingExpiration = true;
-                options.Cookie.SameSite = SameSiteMode.None;
-            });
+                services.AddAuthentication(options =>
+                    {
+                        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                        options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                        options.DefaultChallengeScheme = WsFederationDefaults.AuthenticationScheme;
+                        options.DefaultSignOutScheme = WsFederationDefaults.AuthenticationScheme;
+                    })
+                    .AddWsFederation(options =>
+                    {
+                        options.Wtrealm = authenticationConfiguration.Wtrealm;
+                        options.MetadataAddress = authenticationConfiguration.MetadataAddress;
+                        options.UseTokenLifetime = false;
+                    }).AddCookie(options =>
+                    {
+                        options.AccessDeniedPath = new PathString("/Error/403");
+                        options.ExpireTimeSpan = TimeSpan.FromHours(1);
+                        options.Cookie.Name = CookieName;
+                        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                        options.SlidingExpiration = true;
+                        options.Cookie.SameSite = SameSiteMode.None;
+                    });
+            }
 
             return services;
         }
--- src/SFA.DAS.Tools.Support.Web/Controllers/SupportController.cs
+++ src/SFA.DAS.Tools.Support.Web/Controllers/SupportController.cs
@@ -1,10 +1,6 @@
 ﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.WsFederation;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 using SFA.DAS.Tools.Support.Web.Infrastructure;
 using SFA.DAS.Tools.Support.Web.Models;
 
@@ -12,23 +8,10 @@ namespace SFA.DAS.Tools.Support.Web.Controllers
 {
     public class SupportController : Controller
     {
-        private readonly string _baseUrl;
         private IAuthorizationService _authorizationService;
 
-        public SupportController(ILogger<SupportController> logger,
-            IConfiguration _configuration,
-            IAuthorizationService authorizationService)
+        public SupportController(IAuthorizationService authorizationService)
         {
-            var baseUrl = _configuration.GetValue<string>("BaseUrl");
-            if (!baseUrl.EndsWith('/'))
-            {
-                _baseUrl = string.Concat(baseUrl, '/');
-            }
-            else
-            {
-                _baseUrl = baseUrl;
-            }
-
             _authorizationService = authorizationService;
         }
 
@@ -41,20 +24,6 @@ namespace SFA.DAS.Tools.Support.Web.Controllers
             };
 
             return View(indexViewModel);
-        }
-
-        [AllowAnonymous]
-        public IActionResult LogOut()
-        {
-            foreach (var cookie in Request.Cookies.Keys)
-            {
-                Response.Cookies.Delete(cookie);
-            }
-
-            return SignOut(new Microsoft.AspNetCore.Authentication.AuthenticationProperties
-            {
-                RedirectUri = $"{_baseUrl}Support/LoggedOut"
-            }, CookieAuthenticationDefaults.AuthenticationScheme, WsFederationDefaults.AuthenticationScheme);
         }
 
         [AllowAnonymous]
