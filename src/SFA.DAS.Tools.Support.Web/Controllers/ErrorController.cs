670a54915eaa95cd1ecbf06b53baf029d87b9b40
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2023-10-11 11:34:08 +0100

    Commit Details: 1. Amended Access Denied Page for DfESignIn Users. 2. Unit Testing included with the test coverage for newly added code. 3. DfESignIn Auth Pkg upgraded to the Latest v17.1.56
    Story: https://skillsfundingagency.atlassian.net/browse/FAI-937

--- src/SFA.DAS.Tools.Support.Web/Controllers/ErrorController.cs
+++ src/SFA.DAS.Tools.Support.Web/Controllers/ErrorController.cs
@@ -1,15 +1,31 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using SFA.DAS.Tools.Support.Web.Configuration;
+using SFA.DAS.Tools.Support.Web.Models.Error;
+
 namespace SFA.DAS.Tools.Support.Web.Controllers
 {
     [AllowAnonymous]
     [Route("/error")]
     public class ErrorController : Controller
     {
+        private readonly IConfiguration _configuration;
+        private readonly DfESignInConfig _dfESignInOptions;
+
+        public ErrorController(
+            IOptions<DfESignInConfig> dfESignInOptions,
+            IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _dfESignInOptions = dfESignInOptions.Value;
+        }
+
         [Route("403")]
         public IActionResult AccessDenied()
         {
-            return View();
+            return View(new Error403ViewModel(_configuration["ResourceEnvironmentName"]) { UseDfESignIn = _dfESignInOptions.UseDfESignIn });
         }
 
         [Route("404")]
