8b528fe5809be455ceaf4cccf5e57ee0c12f9c72
Jon Harris <jon.harris@fuitsu.com>
2025-02-03 07:05:31 +0000

    Removing nlog/redis logging

--- src/SFA.DAS.Tools.Support.Web/Program.cs
+++ src/SFA.DAS.Tools.Support.Web/Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using NLog;
-using NLog.Web;
 
 namespace SFA.DAS.Tools.Support.Web;
 
@@ -9,9 +7,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-        LogManager.Setup().LoadConfigurationFromXml(env == "Development" ? "nlog.Development.config" : "nlog.config");
-        
         CreateHostBuilder(args).Build().Run();
     }
 
@@ -20,5 +15,5 @@ public class Program
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
-            }).UseNLog();
+            });
 }
