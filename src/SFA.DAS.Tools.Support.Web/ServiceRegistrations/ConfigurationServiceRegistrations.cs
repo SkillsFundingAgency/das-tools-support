bbeb68a61a506ec77a665eb6a156d043d8711f8b
ogrwill <will.ogrady@digital.education.gov.uk>
2025-02-21 10:35:15 +0000

    adding logging for deployment bug

--- src/SFA.DAS.Tools.Support.Web/ServiceRegistrations/ConfigurationServiceRegistrations.cs
+++ src/SFA.DAS.Tools.Support.Web/ServiceRegistrations/ConfigurationServiceRegistrations.cs
@@ -7,7 +7,6 @@ using SFA.DAS.EmployerUsers.Api.Client;
 using SFA.DAS.Encoding;
 using SFA.DAS.Tools.Support.Infrastructure.Configuration;
 using SFA.DAS.Tools.Support.Web.Configuration;
-using SFA.DAS.Tools.Support.Web.Extensions;
 
 namespace SFA.DAS.Tools.Support.Web.ServiceRegistrations;
 
@@ -35,14 +34,18 @@ public static class ConfigurationServiceRegistrations
         });
 
         services.Configure<CommitmentsClientApiConfiguration>(configuration.GetSection("CommitmentsClientApiConfiguration"));
-        var claimsConfig = new ClaimsConfiguration();
 
+        var claimsConfig = new ClaimsConfiguration();
         services.AddSingleton<IOptions<ClaimsConfiguration>>(new OptionsWrapper<ClaimsConfiguration>(claimsConfig));
 
         services.Configure<ToolsSupportOuterApiConfiguration>(configuration.GetSection(nameof(ToolsSupportOuterApiConfiguration)));
         services.AddSingleton(cfg => cfg.GetService<IOptions<ToolsSupportOuterApiConfiguration>>().Value);
 
         var encodingConfigJson = configuration.GetSection("SFA.DAS.Encoding").Value;
+        if (string.IsNullOrEmpty(encodingConfigJson))
+        {
+            throw new ArgumentNullException("SFA.DAS.Encoding", "SFA.DAS.Encoding Configuration value cannot be null or empty");
+        }
         var encodingConfig = JsonConvert.DeserializeObject<EncodingConfig>(encodingConfigJson);
         services.AddSingleton(encodingConfig);
 
